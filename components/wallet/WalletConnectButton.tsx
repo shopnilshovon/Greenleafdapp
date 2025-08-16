531ea54146729735bdac67ef528d60cd0d600215
SHOVON <141106301+shopnilshovon@users.noreply.github.com>
2025-08-16 22:26:20 +0600

    Create WalletConnectButton.tsx

---
+++ components/wallet/WalletConnectButton.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export function WalletConnectButton() {
+  return (
+    <button className="px-4 py-2 bg-green-500 text-black font-semibold rounded-lg shadow hover:bg-green-400 transition">
+      Connect Wallet
+    </button>
+  );
+}
